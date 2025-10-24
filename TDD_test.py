e929a0ac5e0899dcbb5a68fbbd7b39a76cc95b46
JaretSolomon <jsolomon4@tulane.edu>
2025-10-24 17:51:15 +0000

    done

--- TDD_test.py
+++ TDD_test.py
@@ -1,4 +1,4 @@
-def test_read()
+def test_read():
     assert read(inventory.json) == [ { "name": "Mechanical Keyboard", "price": 120.50, "count": 5 }, 
 { "name": "Wireless Mouse", "price": 45.99, "count": 12 }, 
 { "name": "4K Monitor (27 inch)", "price": 349.00, "count": 3 }, 
@@ -6,7 +6,7 @@ def test_read()
 { "name": "Ergonomic Chair", "price": 280.75, "count": 1 } ]
 
 
-def test_str()
+def test_str():
     assert inventory.str() == print([['Mechanical Keyboard', 120.50, 5],
     ['Wireless Mouse', 45.99, 12],
      ['4K Monitor (27 inch)', 249.00, 3],
